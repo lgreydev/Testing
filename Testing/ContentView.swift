c2402cab5ceb6e62ce38f4f087019630fc48a3eb
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-12-01 20:54:56 -0500

    Initial Commit

---
+++ Testing/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Testing
+//
+//  Created by Sergey Lukaschuk on 2022-12-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Sergey Lukaschuk on 2022-12-01.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
