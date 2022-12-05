82b7d41efcea5f03b887cfa5c68937d6c10846a4
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-12-05 03:32:05 -0500

    Add icon view

--- Testing/ContentView.swift
+++ Testing/ContentView.swift
@@ -9,15 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            Icon(name: "camera.macro.circle.fill")
     }
 }
+
+struct Icon: View {
+    var name: String
+    var tintColor: Color = .blue
+
+    var body: some View {
+        Image(systemName: name)
+            .foregroundColor(tintColor)
+            .frame(width: 100, height: 100)
+    }
+}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
